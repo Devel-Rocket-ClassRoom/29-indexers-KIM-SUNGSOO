2f2d2658f8cdc7d2b36228acd3d4de884f52f660
KIM-SUNGSOO <133596829+KIM-SUNGSOO@users.noreply.github.com>
2026-02-26 19:01:51 +0900

    Prac

--- FruitPriceList/Program.cs
+++ FruitPriceList/Program.cs
@@ -6,6 +6,19 @@ using System.Net.Quic;
 
 
 FruitPriceList fruitPriceList = new FruitPriceList(5);
+fruitPriceList.Add("사과", 1500);
+fruitPriceList.Add("바나나", 3000);
+fruitPriceList.Add("딸기", 8000);
+
+Console.WriteLine($"등록된 과일 수: {fruitPriceList.Count}");
+Console.WriteLine($"사과 가격: {fruitPriceList["사과"]}원");
+Console.WriteLine($"바나나 가격: {fruitPriceList["바나나"]}원");
+Console.WriteLine($"포도 가격: {fruitPriceList["포도"]}원");
+
+for ( int i = 0; i < fruitPriceList.Count; i++ )
+{
+    Console.WriteLine($" {i}번: {fruitPriceList[i]} ");
+}
 class FruitPriceList
 {
     private string[] _names;
@@ -18,15 +31,38 @@ class FruitPriceList
         _prices = new int[capacity];
         
     }
+    
+    public int Count
+    {
+        get { return _count; } 
+    }
+
+    public void Add(string name, int price)
+    {
+        if(_count >= _names.Length)
+        {
+            Console.WriteLine("가격표가 가득 찼습니다.");
+            return;
+        }
+        _names[_count] = name;
+        _prices[_count] = price;
+
+        
+        _count++;
+
+    }
 
     public int this[string name]
     {
         get {
-                if( _names == null)
+                for (int i = 0; i < _count; i++)
                 {
-                return -1;
+                if (_names[i] == name)
+                   {
+                    return _prices[i];     
+                   }
                 }
-                return _prices[_count];
+            return -1;
             }
     }
     
